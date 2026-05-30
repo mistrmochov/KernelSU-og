f25380dbc3f2f9c351c340ff3f03f9fcf7c94c7f
mistrmochov <matejdvoracek@fastmail.com>
2026-05-30 17:39:00 +0200

    [waydroid] Do not mark process on initialization

--- kernel/hook/syscall_hook_manager.c
+++ kernel/hook/syscall_hook_manager.c
@@ -61,10 +61,10 @@ static int syscall_regfunc_handler(struct kretprobe_instance *ri, struct pt_regs
     ksu_tp_marker_lock(&flags);
     if (ksu_tp_marker_reg_count() < 1) {
         // while install our tracepoint, mark our processes
-        ksu_mark_running_process_locked();
+        //ksu_mark_running_process_locked();
     } else if (ksu_tp_marker_reg_count() == 1) {
         // while other tracepoint first added, mark all processes
-        ksu_mark_all_process();
+        //ksu_mark_all_process();
     }
     ksu_tp_marker_inc_reg_count();
     ksu_tp_marker_unlock(&flags);
